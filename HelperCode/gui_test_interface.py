7405983d403b3b8c9c6376abceedd8dfc66731a5
ILBSPT <andremanada@gmail.com>
2024-02-17 21:16:24 +0000

    getter gui, sync for commands

--- HelperCode/gui_test_interface.py
+++ HelperCode/gui_test_interface.py
@@ -7,7 +7,7 @@ import serial
 #windows
 ser = serial.Serial('COM3', 115200, timeout=0.1) #set read timeout of 1s
 
-log_delay = 0.2
+log_delay = 0.05
 
 command_map = {
     "STATUS" : 0,
@@ -39,6 +39,26 @@ state_map_to_string = {
     5: "IMU_CALIB"
 }
 
+sync_state = 1
+cmd_state = 2
+size_state = 3
+data_state = 4
+crc_1_state = 5
+crc_2_state = 6
+end_state = 7
+
+waiting_input = 1
+waiting_rep = 2
+
+comm_stage = waiting_input
+
+comm_state = sync_state
+data_total = 0
+data_recv = 0
+begin = time.perf_counter()
+end = time.perf_counter()
+buff = bytearray()
+
 sg.theme('DarkAmber')    # Keep things interesting for your users
 
 layout = [[sg.Button('LED_ON', key = '_LED_ON_', size = (10,5)), sg.Button('LED_OFF', key = '_LED_OFF_', size = (10,5)), sg.Text("Ax  Ay  Az  Gx  Gy  Gz", key = '_IMU_OUT_', size = (40, 5), auto_size_text=True, font=('Arial Bold', 18))],      
@@ -59,54 +79,50 @@ def toggle_status():
     elif log_status == 1: log_status = 0
 
 def arm():
+    global comm_stage
+
     cmd = bytearray([0x55, command_map['ARM'], 1, 1, 2, 3])
     ser.write(cmd)
     
-    rep = ser.read(6)
-    if(len(rep) != 6 or int(rep[1]) != command_map['ARM_ACK'] or int(rep[3]) != 1):
-        print("ARM1 error", rep)
+    #rep = ser.read(6)
+    read_cmd()
+    if(len(buff) != 6 or int(buff[1]) != command_map['ARM_ACK'] or int(buff[3]) != 1):
+        print("ARM1 error", buff)
+        comm_stage = waiting_input
         return
     else:
         print("ARM 1 ok")
 
     cmd = bytearray([0x55, command_map['ARM'], 1, 2, 2, 3])
     ser.write(cmd)
-    
-    rep = ser.read(6)
-    if(len(rep) != 6 or int(rep[1]) != command_map['ARM_ACK'] or int(rep[3]) != 2):
-        print("ARM2 error", rep)
+    read_cmd()
+    if(len(buff) != 6 or int(buff[1]) != command_map['ARM_ACK'] or int(buff[3]) != 2):
+        print("ARM2 error", buff)
+        comm_stage = waiting_input
         return
     else:
         print("ARM 2 ok")
+    
 
     cmd = bytearray([0x55, command_map['ARM'], 1, 3, 2, 3])
     ser.write(cmd)
-    
-    rep = ser.read(6)
-    if(len(rep) != 6 or int(rep[1]) != command_map['ARM_ACK'] or int(rep[3]) != 3):
-        print("ARM3 error", rep)
+    read_cmd()
+    if(len(buff) != 6 or int(buff[1]) != command_map['ARM_ACK'] or int(buff[3]) != 3):
+        print("ARM3 error", buff)
+        comm_stage = waiting_input
         return
     else:
         print("ARM 3 ok")
+    
 
+    comm_stage = waiting_input
 
-
-sync_state = 1
-cmd_state = 2
-size_state = 3
-data_state = 4
-crc_1_state = 5
-crc_2_state = 6
-end_state = 7
-
-comm_state = sync_state
-data_total = 0
-data_recv = 0
-begin = time.perf_counter()
-end = time.perf_counter()
-buff = bytearray()
 
 def print_status():
+    if(len(buff) < 16):
+        print("bad status")
+        return
+
     state = int.from_bytes(buff[3:4], byteorder='big', signed=True)
 
     ax = int.from_bytes(buff[4:6], byteorder='big', signed=True)
@@ -121,7 +137,7 @@ def print_status():
     gy = int.from_bytes(buff[12:14], byteorder='big', signed=True)
     gz = int.from_bytes(buff[14:16], byteorder='big', signed=True)
 
-
+    print("state", state)
     s1 = "State: " + state_map_to_string[state] + "\n"
     s = s1 + "Ax: " + str(round(ax, 2)) + " Ay: " + str(round(ay, 2)) + " Az: " + str(round(az, 2)) + "\nGx: " + str(round(gx, 2)) + " Gy: " + str(round(gy, 2)) + " Gz: " + str(round(gz, 2))
     window['_IMU_OUT_'].update(s)
@@ -130,74 +146,83 @@ def print_status():
     print("gx:", gx, "gy:", gy, "gz:", gz)
 
 def read_cmd():
+    global comm_stage
     global comm_state, sync_state, cmd_state, size_state, data_state, crc_1_state, crc_2_state, end_state
     global buff
     global data_recv, data_total
     global begin, end
-    
-    while ser.in_waiting > 0:
-        ch = ser.read(1)
-        ch = int.from_bytes(ch, 'little')
 
-        if comm_state == sync_state:
-            if ch == 0x55:
-                comm_state = cmd_state
-                buff = bytearray()
+    buff = bytearray()
+    data_recv = 0
+    begin = time.perf_counter()
+    
+    while True: 
+        while ser.in_waiting > 0:
+            ch = ser.read(1)
+            ch = int.from_bytes(ch, 'little')
+
+            if comm_state == sync_state:
+                if ch == 0x55:
+                    comm_state = cmd_state
+                    buff.append(ch)
+                else:
+                    print(chr(ch), end="")
+                    #print(str(ch).encode('utf-8'))
+
+            elif comm_state == cmd_state:
                 buff.append(ch)
-                data_recv = 0
-                begin = time.perf_counter()
-            else:
-                print(chr(ch), end="")
-                #print(str(ch).encode('utf-8'))
+                comm_state = size_state
 
-        elif comm_state == cmd_state:
-            buff.append(ch)
-            comm_state = size_state
+            elif comm_state == size_state:
+                buff.append(ch)
+                data_total = int(ch)
+                if data_total == 0:
+                    comm_state = crc_1_state
+                else:
+                    comm_state = data_state
 
-        elif comm_state == size_state:
-            buff.append(ch)
-            data_total = int(ch)
-            if data_total == 0:
-                comm_state = crc_1_state
-            else:
-                comm_state = data_state
+            elif comm_state == data_state:
+                buff.append(ch)
+                data_recv += 1
+                if data_total == data_recv:
+                    comm_state = crc_1_state
 
-        elif comm_state == data_state:
-            buff.append(ch)
-            data_recv += 1
-            if data_total == data_recv:
-                comm_state = crc_1_state
+            elif comm_state == crc_1_state:
+                buff.append(ch)
+                comm_state = crc_2_state
 
-        elif comm_state == crc_1_state:
-            buff.append(ch)
-            comm_state = crc_2_state
+            elif comm_state == crc_2_state: 
+                buff.append(ch)
+                comm_state = end_state
+        
+        end = time.perf_counter()
+        msec = (end - begin) 
+        if(comm_stage != sync_state): print("ms: ", msec)
+        if comm_state != sync_state and msec > 1:
+            comm_state = sync_state
+            print("command timeout", msec)
+            print("buffer recieved:", buff) 
+            return
 
-        elif comm_state == crc_2_state: 
-            buff.append(ch)
-            comm_state = end_state
-    
-    end = time.perf_counter()
-    msec = (end - begin) * 1000
-    #print("ms: ", msec)
-    if comm_state != sync_state and msec > 15:
-        comm_state = sync_state
-        print("command timeout", msec)
-        print("buffer recieved:", buff) 
+        if msec > 1:
+            print("no cmd")
+            return
 
-    if comm_state == end_state:
-        comm_state = sync_state
-        print("ACK recieved:", len(buff))
-        if(buff[1] == command_map['STATUS_ACK']):
-            print_status()
-        print("Cmd_ACK: [",''.join('{:02x} '.format(x) for x in buff)[:-1], "]")
-        #print(buff)
+        if comm_state == end_state:
+            comm_state = sync_state
+            print("ACK recieved:", len(buff))
+            if(buff[1] == command_map['STATUS_ACK']):
+                print_status()
+            print("Cmd_ACK: [",''.join('{:02x} '.format(x) for x in buff)[:-1], "]")
+            #print(buff)
+            return
 
 
 log_begin = time.perf_counter()
 log_end = time.perf_counter()
 
 while True:           
-    event, values = window.read(timeout=3) 
+    event, values = window.read(timeout=1) 
     if event != '__TIMEOUT__': print(event, values)       
     cmd_launched = 0
 
@@ -209,11 +234,13 @@ while True:
         if event == '_ARM_':
             cmd_launched = 1
             arm()
+            #read_cmd()
             pass
         elif event == '_READY_':
             cmd_launched = 1
             cmd = bytearray([0x55, command_map['READY'], 0, 0x20, 0x21])
             ser.write(cmd)
+            read_cmd()
             #rep = ser.read(5)
             #print(rep.decode('utf-8'))
             #print(bytearray(rep).hex())
@@ -222,6 +249,7 @@ while True:
             cmd_launched = 1
             cmd = bytearray([0x55, command_map['ABORT'], 0, 0x20, 0x21])
             ser.write(cmd)
+            read_cmd()
             #rep = ser.read(5)
             #print(rep.decode('utf-8'))
             #print(bytearray(rep).hex())
@@ -230,6 +258,7 @@ while True:
             cmd_launched = 1
             cmd = bytearray([0x55, command_map['LED_ON'], 0, 0x20, 0x21])
             ser.write(cmd)
+            read_cmd()
             #rep = ser.read(5)
             #print(rep.decode('utf-8'))
             #print(bytearray(rep).hex())
@@ -237,6 +266,7 @@ while True:
             cmd_launched = 1
             cmd = bytearray([0x55, command_map['LED_OFF'], 0, 0x20, 0x21])
             ser.write(cmd)
+            read_cmd()
             #rep = ser.read(5)
             #print(rep.decode('utf-8'))
             #print(bytearray(rep).hex())
@@ -244,6 +274,7 @@ while True:
             cmd_launched = 1
             cmd = bytearray([0x55, command_map['STATUS'], 0, 0x20, 0x21])
             ser.write(cmd)
+            read_cmd()
             #rep = ser.read(6)
             #print(rep.decode('utf-8'))
             #print(bytearray(rep).hex())
@@ -252,20 +283,21 @@ while True:
             cmd_launched = 1
             cmd = bytearray([0x55, command_map['IMU_CALIB'], 0, 0x20, 0x21])
             ser.write(cmd)
+            read_cmd()
 
     log_end = time.perf_counter()
     if(cmd_launched == 0 and log_status == 1 and log_end - log_begin > log_delay):
         #send status cmd
         cmd = bytearray([0x55, command_map['STATUS'], 0, 0x20, 0x21])
         ser.write(cmd)
+        read_cmd()
         log_begin = time.perf_counter()
         #pass
     
     #while ser.in_waiting > 0:
         #print(ser.read(1).decode('utf-8'), end='')
 
-    read_cmd()
 
-    time.sleep(log_delay * 0.5)
+    time.sleep(log_delay * 0.1)
 
 window.close()
